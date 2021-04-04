954bb3a5bc62e2be3867e1c1fbc575dc015675c6
Vladimir Alkin <71589863+VladimirAlkin@users.noreply.github.com>
2021-04-04 14:02:23 +0300

    .py files
    Sane project but PyCharm structured

---
+++ texttw.py
@@ -0,0 +1,18 @@
+import re
+# Create a function to clean the tweets
+def cleanTxt(text):
+    text = re.sub('@[A-Za-z0–9]+', '', text)  # Removing @mentions
+    text = re.sub('#', '', text)  # Removing '#' hash tag
+    text = re.sub('RT[\s]+', '', text)  # Removing RT
+    text = re.sub('https?:\/\/\S+', '', text)  # Removing hyperlink
+
+    return text
+
+# Create a function to compute negative (-1), neutral (0) and positive (+1) analysis
+def getAnalysis(score):
+  if score < 0:
+    return 'Negative'
+  elif score == 0:
+    return 'Neutral'
+  else:
+    return 'Positive'
